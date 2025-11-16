61ba2bbd48ad46da89402818d91c70fefe78f340
Ankita Satish Kumar <ankitasatish0711@gmail.com>
2025-11-16 19:26:02 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function Header({ onHome, onLogout }) {
+export default function Header({ onHome, onLogout, isDarkMode, setIsDarkMode }) {
   return (
     <header className="header">
-      <h1>Lost & Found</h1>
+      <h1>🔍 back2you</h1>
       <div className="header-buttons">
+        <button onClick={() => setIsDarkMode(!isDarkMode)}>
+          {isDarkMode ? "☀️" : "🌙"}
+        </button>
         <button onClick={onHome}>🏠 Home</button>
         <button onClick={onLogout}>🚪 Logout</button>
       </div>
